80970cd810e19c10c0d4f430ea688d6c7eb6a316
SH-Codes <wldrms@outlook.com>
2023-08-11 15:24:50 -0700

    1-last_digit.c: This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print the last digit of the number stored in the variable n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Return: 0 
+ * Return: 0 Always (Success)
 */
 
 int main(void)
@@ -21,6 +21,6 @@ int main(void)
 	if (m == 0)
 		printf("Last digit of %d is %d and is 0\n", n, m);
 	if (m < 6 && m != 0)
-		printf("Last digit of %d is %d and is less than 6 not 0\n", n, m);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 	return (0);
 }
