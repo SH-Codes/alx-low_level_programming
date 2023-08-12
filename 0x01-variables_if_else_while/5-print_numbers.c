e3af2f773780e30de60860318493b69a7c5bfa03
SH-Codes <wldrms@outlook.com>
2023-08-11 20:38:38 -0700

    5-print_numbers.c:  prints all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* Task 5:  prints all single digit numbers of base 10 start: 0 */
+/**
+ * main - Entry point
+ * Return: 0 Always (Success)
+ */
+
+int main(void)
+{
+	char base_ten = '0';
+
+	while (base_ten <= '9')
+	{	
+		putchar(base_ten);
+		base_ten++;
+	}
+	putchar('\n');
+	return (0);
+}
