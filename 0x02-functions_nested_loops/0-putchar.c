e563b2fb01b575b96b09c5753e8e54e7771bcb2c
SH-Codes <wldrms@outlook.com>
2023-08-15 17:57:40 -0700

    0-putchar.c:prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
-#include <stdio.h>
+#include <main.h>
 /**
- * main - print _putchar
- * Return: 0 Always (Success)
- */
+* main - print _putchar
+* Return: 0 Always (Success)
+*/
 
 int main(void)
 {
-	int i;
-	char c[] = "_putchar";
-
-	for (i = 0; i <= 7; i++)
-	{
-		putchar(c[i]);
-	}
-
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
