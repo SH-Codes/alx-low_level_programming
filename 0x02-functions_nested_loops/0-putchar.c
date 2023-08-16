85b61c844d45752fe3ef1ebaa77344920a229665
SH-Codes <wldrms@outlook.com>
2023-08-16 13:53:30 -0700

    0-putchar.c: prints the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 #include <stdio.h>
-
-=======
 #include "main.h"
->>>>>>> eec53d71e0f2f6672a9ff8a65f9181b99882751b
 /**
  * main - print _putchar
  * by Sihle Hlophe
@@ -19,7 +15,7 @@ int main(void)
 	{
 		putchar(c[i]);
 	}
-	putchar('\n')
+	putchar('\n');
 
 	return (0);
 }
