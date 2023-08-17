c051b03acf150d024ce682f4fadda62c75e946e5
SH-Codes <wldrms@outlook.com>
2023-08-17 05:00:52 +0200

    6-abs.c: computes the absolute value of an integer.

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value if an integer
+ * @n: the int checks
+ * Returm: the absolute value of int
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+		return (-n);
+}
