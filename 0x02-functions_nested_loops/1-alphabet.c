b47b997b4163569debc90d5a88a8ac1e9ec335fc
SH-Codes <wldrms@outlook.com>
2023-08-17 02:25:46 +0200

    1-alphabet.c: prints the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- * main - prints alphabets in lowercase
- * Return: void
+ * print_alphabet - prints the alphabet in lowercase.
+ *
+ * This function will print the alphabet in lowercase, 
+ * followed by a newline. It does not return any value.
  */
 void print_alphabet(void)
 {
-	char al;
-	for (al = 'a'; al <= 'z'; al++)
-		_putchar(al);
-	_putchar('\n');
+    char al;
+    for (al = 'a'; al <= 'z'; al++)
+        _putchar(al);
+    _putchar('\n');
 }
