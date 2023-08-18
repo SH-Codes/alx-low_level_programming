f221e439562eac07e8f998484ef6e6c63fab69c4
SH-Codes <wldrms@outlook.com>
2023-08-18 05:10:36 +0200

    2-mul.c: multiplies two integers.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * int 2-mul - multiplies two integers
+ * @a: parameter 1
+ * @b: parameter 2
+ * by Sihle Hlophe (SH-Codes)
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
