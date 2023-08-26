2e5dc380bd44a38d05b85ffd7e71798dfdfc2a6e
SH-Codes <wldrms@outlook.com>
2023-08-26 03:28:16 +0200

    1-strncat.c:  concatenates two strings.

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,29 +1,30 @@
 #include "main.h"
 /**
- * _strcat - concatenates two strings
- * @dest: char type paramater
- * @src: char type parameter
- * Return: char
+ * _strncat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ * @n: number of characters to concatenate
+ * Return: pointer to destination string
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-        int a;
-        int b;
+	int a;
+	int b;
 
-        a = 0;
-        while (dest[a] != '\0')
-        {
-                a++;
-        }
-        b = 0;
-        while (b < n && src[b] != '\0')
-        {
-                dest[a] = src[b];
-                a++;
-                b++;
-        }
+	a = 0;
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
 
-        dest[a] = '\0';
-        return (dest);
+	b = 0;
+	while (b < n && src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+	return (dest);
 }
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string
+ * @dest: char type parameter
+ * @src: char type parameter
+ * @n: int parameter
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (dest);
+}
