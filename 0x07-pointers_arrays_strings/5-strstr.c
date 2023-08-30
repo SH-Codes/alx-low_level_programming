c1c709facdda07bc809654dd8092e13a369928c7
SH-Codes <wldrms@outlook.com>
2023-08-30 03:18:43 +0200

    5-strstr.c: locates a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strstr - locates a substring
+ * @haystack: char type pointer
+ * @needle: char ttype pointer
+ * Return: NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	if (!*needle)
+	{
+		return (haystack);
+	}
+
+	for (; *haystack; haystack++)
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*h && *n && (*h == *n))
+		{
+			h++;
+			n++;
+		}
+
+		if (!*n)
+		{
+			return (haystack);
+		}
+	}
+	return (NULL);
+}
