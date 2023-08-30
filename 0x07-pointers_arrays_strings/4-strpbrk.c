24037d6cb3f65400724459859066a8fa66c948f0
SH-Codes <wldrms@outlook.com>
2023-08-30 03:05:16 +0200

    4-strpbrk.c: searches a string for any of a set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: char type pointer
+ * @accept: char type pointer
+ * Return: NULL
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	for (; *s; s++)
+	{
+		char *a = accept;
+
+		while (*a)
+		{
+			if (*s == *a)
+			{
+				return (s);
+			}
+			a++;
+		}
+	}
+	return (NULL);
+}
