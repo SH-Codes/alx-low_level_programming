8d188a57377a8069b1bef7b75d2f44f6eff3279d
SH-Codes <wldrms@outlook.com>
2023-09-08 05:36:34 +0200

    3-array_range.c: creates an array of integers

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+int *array_range(int min, int max)
+{
+	int length;
+	int *arr;
+	int i;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	length = max - min + 1;
+	arr = (int *)malloc(length * sizeof(int));
+
+	if (!arr)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < length; i++)
+	{
+		arr[i] = min + i;
+	}
+	return (arr);
+}
