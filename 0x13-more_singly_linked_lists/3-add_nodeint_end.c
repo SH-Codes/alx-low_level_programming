10d08ab7689489301de03350ec94b4fa6262cf35
SH-Codes <wldrms@outlook.com>
2023-09-27 00:53:09 +0200

    3-add_nodeint_end.c: contains a function that adds a new node at the end of a listint_t list.

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ *  add_nodeint_end - Appends a new node to the end of a linked list.
+ *  @head: A double pointer to the head of the linked list.
+ *  @n: Value to be stored in the new node.
+ *  Return: Pointer to the newly created node.
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_node;
+	listint_t *current_node;
+
+	new_node = malloc(sizeof(listint_t));
+
+	if (!new_node)
+	{
+		return (NULL);
+	}
+
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (!*head)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	current_node = *head;
+
+	while (current_node->next)
+	{
+		current_node = current_node->next;
+	}
+	current_node->next = new_node;
+	return (new_node);
+}
