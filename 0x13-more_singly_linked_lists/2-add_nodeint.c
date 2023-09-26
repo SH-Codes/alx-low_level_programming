fdd6cdb1112168eb06417453999d8cc876aef653
SH-Codes <wldrms@outlook.com>
2023-09-26 23:54:44 +0200

    2-add_nodeint.c: contains a function that adds a new node at the beginning of a listint_t list.

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - Adds a new node at the beginning of a linked list.
+ * @head: A double pointer to the start of the linked list.
+ * @n: Value to be stored in the new node
+ * Return: Pointer to the new node, or NULL if the function fails.
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node = malloc(sizeof(listint_t));
+
+	if (!new_node)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
